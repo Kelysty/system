803a967652f5666788ae0f0d4255d59a28d9efc2
Ivan Postarnak <ivan.postarnak.s@gmail.com>
2023-12-01 12:46:08 +0600

    test(build): add passing with 0 on lacking tests

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   verbose: true,
   moduleFileExtensions: ['js', 'jsx', 'json', 'ts', 'tsx'],
+  passWithNoTests: true,
   rootDir: '.',
   transform: {
       '^.+\\.tsx?$': 'ts-jest',
